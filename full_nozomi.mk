2558bfc31fae55adebfb65ea644f1ed6b60bd72e
Fehér NeoH <feherneoh@gmail.com>
2013-08-30 08:44:06 +0200

    Initial commit for Firefox OS

--- full_nozomi.mk
+++ full_nozomi.mk
@@ -21,11 +21,6 @@
 # lines, full and maguro, hence its name.
 #
 #
- 
-# Camera & Torch
-PRODUCT_PACKAGES := \
-    Camera \
-    Torch
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
